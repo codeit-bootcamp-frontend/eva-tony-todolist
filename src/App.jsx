44b99bed92d32e851d5f7a2e0555b695d39beba8
Yejin Shin <shinyejin0212@gmail.com>
2023-05-03 15:40:56 +0900

    chore: 초기 설정
    - 1. package 설정 react-calendar, react-router-dom 설치
    - 2. path aliasing 설정
    - 3. global css 설정
    - 4. router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Calendar from "react-calendar";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
