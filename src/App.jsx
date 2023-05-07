3eb9b27387ed8972ffe51845d8badfa699e6b41e
Yejin Shin <shinyejin0212@gmail.com>
2023-05-08 00:44:05 +0900

    fix: auth redirect url 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Layout from "@pages/Layout";
 import Auth from "@pages/Auth";
 import { Routes, Route } from "react-router-dom";
 import User from "@pages/User";
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const CLIENT_URL = import.meta.env.VITE_CLIENT_URLL;
 
 const REST_API_KEY = "75796b33aac62e190e50bcc064cce581";
-const REDIRECT_URI = `${BASE_URL}oauth/kakao/callback`;
+const REDIRECT_URI = `${CLIENT_URL}oauth/kakao/callback`;
 const KAKAO_REDIRECT_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 function App() {
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useCallback } from "react";
 import qs from "qs";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const CLIENT_URL = import.meta.env.VITE_CLIENT_URLL;
 
 const REST_API_KEY = "75796b33aac62e190e50bcc064cce581";
-const REDIRECT_URI = `${BASE_URL}oauth/kakao/callback`;
+const REDIRECT_URI = `${CLIENT_URL}oauth/kakao/callback`;
 const CLIENT_SECRET = "CnTc6hyr4KUvRvUXCesPDAhXXJCQszeZ";
 
 const Auth = ({ setUserInfo }) => {
