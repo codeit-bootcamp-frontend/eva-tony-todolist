0c654518b4480512f54823d42116bedc5f24506b
임병욱 <bwlimtony@gmail.com>
2023-05-08 00:21:22 +0900

    chore: url 수정

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useCallback } from "react";
-import qs from "qs";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useCallback } from 'react';
+import qs from 'qs';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-const REST_API_KEY = "75796b33aac62e190e50bcc064cce581";
+const REST_API_KEY = '75796b33aac62e190e50bcc064cce581';
 const REDIRECT_URI = `${BASE_URL}oauth/kakao/callback`;
-const CLIENT_SECRET = "CnTc6hyr4KUvRvUXCesPDAhXXJCQszeZ";
+const CLIENT_SECRET = 'CnTc6hyr4KUvRvUXCesPDAhXXJCQszeZ';
 
 const Auth = ({ setUserInfo }) => {
   const navigate = useNavigate();
-  const code = new URL(window.location.href).searchParams.get("code");
+  const code = new URL(window.location.href).searchParams.get('code');
 
   const payload = qs.stringify({
-    grant_type: "authorization_code",
+    grant_type: 'authorization_code',
     client_id: REST_API_KEY,
     redirect_uri: REDIRECT_URI,
     code: code,
@@ -24,21 +24,21 @@ const Auth = ({ setUserInfo }) => {
   const kakaoLogin = useCallback(async () => {
     try {
       const res = await axios({
-        method: "post",
+        method: 'post',
         url: `https://kauth.kakao.com/oauth/token`,
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
         },
         data: payload,
       });
-
+      const abd = 3;
       const ACCESS_TOKEN = res.data.access_token;
 
-      localStorage.setItem("accessToken", ACCESS_TOKEN);
+      localStorage.setItem('accessToken', ACCESS_TOKEN);
     } catch (error) {
       console.log(error);
     } finally {
-      navigate("/");
+      navigate('/');
     }
   }, []);
 
