8c7fad559c74bd0fb6c47a3f9f557b75ac5b9377
wooki <bwlimtony@gmail.com>
2023-05-18 21:58:56 +0900

    fix: 경로 수정

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "@components/Header";
-import FooterNav from "@components/FooterNav";
+import Header from "@components/Header/Header.jsx";
+import FooterNav from "@components/FooterNav/Footernav.jsx";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
